5cf9ada44fa416b1ed0a8f11ad71d7b07822b667
Andrea Navarrete <andreanavarrete@Andreas-MacBook-Pro-2.local>
2017-06-28 18:08:50 -0500

    download highways script

--- pipeline/etl/raw/download_highways.sh
+++ pipeline/etl/raw/download_highways.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-TMPDIR="/shapefiles/"
-STATEDIR="/amman_jordan/"
-TABLA="Jordan"
-STATESCHEMA="raw"
+source ../../.env
+
 CITY="amman"
+COUNTRY="jordan"
 
-DB="postgis_test"
-USER_NAME="andreuboadadeatela"
-
-mkdir $TMPDIR
-cd $TMPDIR
-
-# amman
-mkdir $STATEDIR
-cd $STATEDIR
+# create data directory
+mkdir $DATADIR/$CITY
 
 #descarga
-wget https://s3.amazonaws.com/metro-extracts.mapzen.com/amman_jordan.osm2pgsql-shapefiles.zip
-wget https://s3.amazonaws.com/metro-extracts.mapzen.com/amman_jordan.osm2pgsql-geojson.zip
+wget https://s3.amazonaws.com/metro-extracts.mapzen.com/${CITY}_${COUNTRY}.osm2pgsql-shapefiles.zip
 
 #unzip files into directory
-for z in *.zip; do unzip -o -d $TMPDIR $z; done
+for z in *.zip; do unzip -o -d $DATADIR/$CITY $z; done
 
 #delete zip files
 for z in *.zip; do rm $z; done
 
-shp2pgsql -s 4326 -S -W "utf-8" -I -d amman_jordan_osm_line.shp $STATESCHEMA.${CITY}_highways | psql -U $USER_NAME -d $DB
+# upload to postgresql 
+shp2pgsql -s 4326 -d -D -I -W "latin1" ${DATADIR}/${CITY}/${CITY}_${COUNTRY}_osm_line.shp raw.${CITY}_highways | psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER
+
