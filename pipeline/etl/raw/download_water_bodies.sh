83aaa5423fc5fe6e2691055306570645af9b7323
root <root@4751477fa8be>
2017-06-30 18:42:35 +0000

    upload water bodies for amman

--- pipeline/etl/raw/download_water_bodies.sh
+++ pipeline/etl/raw/download_water_bodies.sh
@@ -3,8 +3,7 @@
 . ./../../.env
 
 CITY='amman'
-
-COUNTY_SHP='/home/JOR_adm0.shp'
+COUNTRY='Jordan'
 
 # create data directory
 #mkdir ${DATADIR}/water_bodies
@@ -22,7 +21,7 @@ echo ${DATADIR}
 #ogr2ogr -f "ESRI Shapefile" ${DATADIR}/water_bodies/shps ${DATADIR}/water_bodies/v10/hydropolys.gdb
 
 # clip
-#ogr2ogr -clipsrc ${DATADIR}/water_bodies/shps/hydropolys.shp ${DATADIR}/water_bodies/shps/${CITY}.shp $COUNTY_SHP
+ogr2ogr -clipsrc ${DATADIR}/countries_shp/${COUNTRY}.shp ${DATADIR}/water_bodies/shps/${CITY}.shp ${DATADIR}/water_bodies/shps/hydropolys.shp
 
 # upload to postgresql
-#shp2pgsql -s 4326 -d -D -I -W "latin1" ${DATADIR}/water_bodies/shps/hydropolys.shp raw.${CITY}_water_bodies | psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER
+shp2pgsql -s 4326 -d -D -I -W "latin1" ${DATADIR}/water_bodies/shps/${CITY}.shp raw.${CITY}_water_bodies | psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER
