d3e719cbba5333a155570fd925a3ad0deab61ab8
root <root@4751477fa8be>
2017-06-30 20:51:09 +0000

    add DEM and fix paths for highways and water bodies

---
+++ pipeline/etl/raw/download_dem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. ./../../.env
+
+CITY="amman"
+COUNTRY="jordan"
+
+# create data directory
+mkdir $DATADIR/dem
+
+# descarga
+#### TODO
+
+#unzip files into directory
+for z in $DATADIR/dem/*.zip; do unzip -o -d $DATADIR/dem/  $z; done
+#
+raster2pgsql -d -I -C -M -F -t 100x100 -s 4326 $DATADIR/dem/*_med075.tif raw.${CITY}_dem > $DATADIR/dem/elev.sql
+psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER -f $DATADIR/dem/elev.sql
--- pipeline/etl/raw/download_water_bodies.sh
+++ pipeline/etl/raw/download_water_bodies.sh
@@ -21,7 +21,7 @@ echo ${DATADIR}
 #ogr2ogr -f "ESRI Shapefile" ${DATADIR}/water_bodies/shps ${DATADIR}/water_bodies/v10/hydropolys.gdb
 
 # clip
-ogr2ogr -clipsrc ${DATADIR}/countries_shp/${COUNTRY}.shp ${DATADIR}/water_bodies/shps/${CITY}.shp ${DATADIR}/water_bodies/shps/hydropolys.shp
+ogr2ogr -clipsrc ${DATADIR}/boundries/${COUNTRY}.shp ${DATADIR}/water_bodies/shps/${CITY}.shp ${DATADIR}/water_bodies/shps/hydropolys.shp
 
 # upload to postgresql
 shp2pgsql -s 4326 -d -D -I -W "latin1" ${DATADIR}/water_bodies/shps/${CITY}.shp raw.${CITY}_water_bodies | psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER
