ee838d4d52c3a4924c467fa485fa7ae23f74f6ee
root <root@4751477fa8be>
2017-07-02 21:50:07 +0000

    download dem

--- pipeline/etl/raw/download_dem.sh
+++ pipeline/etl/raw/download_dem.sh
@@ -12,7 +12,7 @@ mkdir $DATADIR/dem
 #### TODO
 
 #unzip files into directory
-for z in $DATADIR/dem/*.zip; do unzip -o -d $DATADIR/dem/  $z; done
+#for z in $DATADIR/dem/*.zip; do unzip -o -d $DATADIR/dem/  $z; done
 #
 raster2pgsql -d -I -C -M -F -t 100x100 -s 4326 $DATADIR/dem/*_med075.tif raw.${CITY}_dem > $DATADIR/dem/elev.sql
 psql -d $PGDATABASE -h $PGHOST -U $POSTGRES_USER -f $DATADIR/dem/elev.sql
