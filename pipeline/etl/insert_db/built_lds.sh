a6c18d0b3412290774cf550b292b1aeedacba732
root <root@4751477fa8be>
2017-08-07 21:44:49 +0000

    Fix path directory

--- pipeline/etl/insert_db/built_lds.sh
+++ pipeline/etl/insert_db/built_lds.sh
@@ -9,10 +9,10 @@ DATADIR=$3
 
 ### aqui
 echo 'transform Countries'
-ogr2ogr -t_srs EPSG:54009 ${DATADIR}/boundries/${CITY}_54009.shp ${DATADIR}/boundries/${CITY}.shp
+ogr2ogr -t_srs EPSG:54009 ${DATADIR}/shp_buffer/${CITY}.shp ${DATADIR}/shp_buffer/${CITY}.shp
 
 echo 'cutting border'
-gdalwarp -cutline ${DATADIR}/boundries/${CITY}_54009.shp -crop_to_cutline -dstalpha $DATADIR/built_lds/$YEAR/GHS_BUILT_LDS${YEAR}_GLOBE_R2016A_54009_250_v1_0/*.tif ${DATADIR}/built_lds/$YEAR/built_lds_${CITY}_54009.tif
+gdalwarp -cutline ${DATADIR}/shp_buffer/${CITY}.shp -crop_to_cutline -dstalpha $DATADIR/built_lds/$YEAR/GHS_BUILT_LDS${YEAR}_GLOBE_R2016A_54009_250_v1_0/*.tif ${DATADIR}/built_lds/$YEAR/built_lds_${CITY}_54009.tif
 
 echo 'tranform'
 gdalwarp  -s_srs EPSG:54009 -t_srs EPSG:4326 ${DATADIR}/built_lds/$YEAR/built_lds_${CITY}_54009.tif ${DATADIR}/built_lds/$YEAR/built_lds_${CITY}.tif
