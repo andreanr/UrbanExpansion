50866e4478b162562427561053bbc111794d23ef
root <root@4751477fa8be>
2017-08-09 19:05:07 +0000

    add update_id property for tasks including parameters

--- pipeline/etl/preprocess.py
+++ pipeline/etl/preprocess.py
@@ -6,7 +6,7 @@ from luigi import configuration
 from luigi.contrib import postgres
 from dotenv import load_dotenv,find_dotenv
 from commons import city_task
-from etl.etl_insertdb import InsertDBTasks
+#from etl.etl_insertdb import InsertDBTasks
 
 load_dotenv(find_dotenv())
 
@@ -16,7 +16,7 @@ class slope(city_task.PostgresTask):
 
     @property
     def update_id(self):
-        return 'slope_raw:{city}'.format(self.city)
+        return 'slope_raw:{city}'.format(city=self.city)
 
     @property
     def table(self):
@@ -24,7 +24,7 @@ class slope(city_task.PostgresTask):
 
     @property
     def query(self):
-        drop = ("""DROP TABLE raw.{city}_slope;""".format(city=self.city))
+        drop = ("""DROP TABLE IF EXISTS raw.{city}_slope;""".format(city=self.city))
         create = ("""CREATE TABLE raw.{city}_slope AS (
                     select
                         st_slope(st_transform(rast, {esri}) ,1,'32BF','PERCENT') as rast_percent,
@@ -33,15 +33,15 @@ class slope(city_task.PostgresTask):
                                                     esri=self.esri))
         return drop + create
 
-    def requires(self):
-        return InsertDBTasks()
+#    def requires(self):
+#        return InsertDBTasks()
 
 class city_center(city_task.PostgresTask):
     city = luigi.Parameter()
 
     @property
     def update_id(self):
-        return 'city_center_raw:{city}'.format(self.city)
+        return 'city_center_raw:{city}'.format(city=self.city)
 
     @property
     def table(self):
@@ -50,21 +50,21 @@ class city_center(city_task.PostgresTask):
     @property
     def query(self):
         # Parameters
-        latitud = configuration.get_config().get(self.city,'latitud')
-        longitude = configuration.get_config().get(self.city,'longitud')
+        latitude = configuration.get_config().get(self.city,'latitude')
+        longitude = configuration.get_config().get(self.city,'longitude')
         buffer_size = configuration.get_config().get(self.city,'buffer_size')
         # drop and create
         drop = """DROP TABLE IF EXISTS raw.{city}_city_center;""".format(city=self.city)
         create = ("""CREATE TABLE raw.{city}_city_center as (
                     SELECT ST_buffer(ST_SetSRID(ST_MakePoint({longitude}, {latitude}), 4326), {buffer_size}) as geom
-                    );""".format(city=self.city_center,
+                    );""".format(city=self.city,
                                  longitude=longitude,
                                  latitude=latitude,
                                  buffer_size=buffer_size))
         return drop + create
 
-    def requires(self):
-        return InsertDBTasks()
+#    def requires(self):
+#        return InsertDBTasks()
 
 
 class PreprocessTask(luigi.WrapperTask):
