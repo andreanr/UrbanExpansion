51d4bd3cebe8d4139a3f2e1fb8ef72c2ac628e27
root <root@e24d0b5cccc1>
2017-09-01 18:58:37 +0000

    -Add installation stuff to Dockerfile. Fix bug in population insertdb.

--- pipeline/etl/insert_db/population.sh
+++ pipeline/etl/insert_db/population.sh
@@ -6,6 +6,12 @@ CITY=$1
 YEAR=$2
 DATADIR=$3
 
+if [ ! -e ${DATADIR}/shp_buffer/${CITY}_54009.shp ]
+then
+        echo 'transform coordinate system'
+        ogr2ogr -s_srs EPSG:4326 -t_srs EPSG:54009 ${DATADIR}/shp_buffer/${CITY}_54009.shp ${DATADIR}/shp_buffer/${CITY}.shp
+fi
+
 echo 'cutting border'
 gdalwarp -cutline ${DATADIR}/shp_buffer/${CITY}_54009.shp -crop_to_cutline -dstalpha $DATADIR/population/$YEAR/GHS_POP_GPW4${YEAR}_GLOBE_R2015A_54009_250_v1_0/*.tif ${DATADIR}/population/$YEAR/population_${CITY}_54009.tif
 
